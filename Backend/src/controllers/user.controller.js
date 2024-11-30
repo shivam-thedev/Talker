9327b810713f6bcb6346e96cc4831a6b208de920
Shivam <kumarshivam3788@gmail.com>
2024-11-30 12:23:52 +0530

    add login functionality and fix password validation in signup

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req,res) => {
             return res.status(400).json({message:"All fields are required"})
         }
 
-        if(password<6){
+        if(password.length<6){
             return res.status(400).json({message:"Password must be at least 6 characters"})
         }
 
@@ -18,8 +18,9 @@ export const signup = async (req,res) => {
         if(existedUser){
             return res.status(400).json({message:"Email already exists"})
         }
-
-        const hashPassword = bcrypt.hash(password,10)
+        
+    
+        const hashPassword = await bcrypt.hash(password,10)
 
         const user = new User({
             fullName,
@@ -42,6 +43,38 @@ export const signup = async (req,res) => {
         }
     } catch (error) {
         console.log("Error in signup",error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const login = async (req,res) => {
+    const {email,password} = req.body
+    try {
+        if(!email && !password){
+            res.status(400).json({message:"all fields are required"})
+        }
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            res.status(400).json({message:"User does not exist"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect){
+            res.status(400).json({message:"Invalid password"})
+        }
+
+        generateToken(user._id,res)
+
+        res.status(400).json({
+            id:user._id,
+            fullName:user.fullName,
+            email:user.fullName,
+            profilePic:user.profilePic
+        })
+    } catch (error) {
+        console.log("Error in login",error.message)
         res.status(500).json({message:"Internal server error"})
     }
 }
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
-import { signup } from "../controllers/user.controller.js"
+import { login, signup } from "../controllers/user.controller.js"
 
 const router = Router()
 
 router.post("/signup",signup)
+router.post("/login",login)
 
 
 export default router
