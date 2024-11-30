a7fef092245c05e83191c7914febce22b4332ec6
Shivam <kumarshivam3788@gmail.com>
2024-11-30 22:01:48 +0530

    add sendMessage functionality and update message routes for sending messages

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import { Message } from "../models/message.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 export const getUserForSidebar = async (req,res) => {
     try {
@@ -27,6 +28,36 @@ export const getMessages = async (req,res) => {
         res.status(200).json(messages)
     } catch (error) {
         console.log("Error in getting messages",error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const sendMessage = async (req,res) =>{
+    try {
+        const {text,image} = req.body
+        const {id:receiverId} = req.params
+        const senderId = req.user._id
+        
+        let imageUrl
+        if(image){
+            const response = await uploadOnCloudinary(image)
+            imageUrl = response.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+
+        await newMessage.save()
+
+        // todo: realtime functionality goes here => socket.io
+        
+        res.status(200).json(newMessage)
+    } catch (error) {
+        console.log("Error in sending messages",error.message)
         res.status(500).json({message:"Internal server error"})
     }
 }
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getMessages, getUserForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = Router()
 
 // Protected routes
 router.get("/users",verifyJWT,getUserForSidebar)
 router.get("/:id",verifyJWT,getMessages)
+router.post("send/:id",verifyJWT,sendMessage)
 
 export default router
